7e879fa18f7ff8d18ae75542df6361c1f6d96b7e
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2022-08-20 00:54:53 +0700

    update: disable register

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [HomeController::class, 'redirect'])->name('home');
 Route::get('/test', [HomeController::class, 'test'])->name('test');
