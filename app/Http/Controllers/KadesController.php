91307979c7afd6c4030f2efc521cc13478c9bb17
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2022-08-22 10:17:39 +0700

    feat: dashboard kades

--- app/Http/Controllers/KadesController.php
+++ app/Http/Controllers/KadesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KadesController extends Controller
 {
@@ -17,9 +19,49 @@ class KadesController extends Controller
     }
 
     /**
-     * @return view dashboard
+     * Menampilkan view dashboard admin
+     * 
+     * @return view
      */
-    public function dashboard() {
-        return view('kades.dashboard');
+    public function dashboard()
+    {
+        $dataPemeriksaanBalita = DB::table('pemeriksaan_balita')
+                ->select('id_pemeriksaan_balita', 'created_at', DB::raw('YEAR(created_at) as tahun, month(created_at) as bulan, count(created_at) as jumlah'))
+                ->groupBy(DB::raw('month(created_at)'))
+                ->where([
+                    ['is_deleted', 0],
+                    ['created_at', '>', Carbon::now()->subYear()]
+                ])->get();
+
+        $dataPemeriksaanLansia = DB::table('pemeriksaan_lansia')
+                ->select('created_at', DB::raw('YEAR(created_at) as tahun, month(created_at) as bulan, count(created_at) as jumlah'))
+                ->groupBy(DB::raw('month(created_at)'))
+                ->where([
+                    ['is_deleted', 0],
+                    ['created_at', '>', Carbon::now()->subYear()]
+                ])->get();
+
+        $dataJumlahPosyandu = DB::table('posyandu')
+                ->select(DB::raw('count(id_posyandu) as jumlah'))
+                ->where('is_deleted', 0)
+                ->get();
+        $dataJumlahBalita = DB::table('balita')
+                ->select(DB::raw('count(id_balita) as jumlah'))
+                ->where('is_deleted', 0)
+                ->get();
+
+        $dataJumlahLansia = DB::table('lansia')
+                ->select(DB::raw('count(id_lansia) as jumlah'))
+                ->where('is_deleted', 0)
+                ->get();
+
+        // dd($dataJumlahBalita);
+        return view('kades.dashboard', compact(
+            'dataPemeriksaanBalita', 
+            'dataPemeriksaanLansia', 
+            'dataJumlahPosyandu',
+            'dataJumlahBalita', 
+            'dataJumlahLansia'
+        ));
     }
 }
